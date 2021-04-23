eee6ca363e37ab24e1bb368486747942fdbd0449
paryanimohit <mohitparyani23@gmail.com>
2021-04-23 15:33:48 -0400

    Added POJO file for fetch rewards

---
+++ rest-service/rest-service/src/main/java/com/fetchRewards/restservice/FetchPoints.java
@@ -0,0 +1,30 @@
+package com.fetchRewards.restservice;
+
+import java.util.Date;
+
+public class FetchPoints {
+
+	private final String payer;
+	private final int points;
+	private final Date timeStamp;
+	
+	public FetchPoints(String payer, int points, Date timeStamp) {
+		super();
+		this.payer = payer;
+		this.points = points;
+		this.timeStamp = timeStamp;
+	}
+
+	public String getPayer() {
+		return payer;
+	}
+
+	public int getPoints() {
+		return points;
+	}
+
+	public Date getTimeStamp() {
+		return timeStamp;
+	}
+	
+}
